95066f5d2f529d46061ebe641c762688ad995741
thxrhmn <rahmanmuhaemin@gmail.com>
2023-10-29 22:19:08 +0700

    change to public

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: 'public',
+			assets: 'public',
 			fallback: undefined,
 			precompress: false,
 			strict: true
